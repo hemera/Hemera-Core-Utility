9b048bbc601fc8dbf5d72c23954867e790111300
Yi Wang <neakor@gmail.com>
2013-04-30 11:41:11 -0400

    Removed parameter parsing in favor of Apache's library.

--- unittest/hemera/core/utility/unittest/TestURIParser.java
+++ unittest/hemera/core/utility/unittest/TestURIParser.java
@@ -3,52 +3,12 @@ package hemera.core.utility.unittest;
 import hemera.core.utility.uri.RESTURI;
 import hemera.core.utility.uri.URIParser;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Random;
 import java.util.UUID;
 
 import junit.framework.TestCase;
 
 public class TestURIParser extends TestCase {
 
-	public void testNoArguments() throws Exception {
-		final String id = UUID.randomUUID().toString().replace("-", "");
-		final String uri = "/resource/"+id+"/action";
-		final Map<String, String> arguments = URIParser.instance.parseURIArguments(uri);
-		assertEquals(null, arguments);
-	}
-	
-	public void testInvalidArguments() throws Exception {
-		final String id = UUID.randomUUID().toString().replace("-", "");
-		final String uri = "/resource/"+id+"/action?";
-		final Map<String, String> arguments = URIParser.instance.parseURIArguments(uri);
-		assertEquals(null, arguments);
-	}
-	
-	public void testHasArguments() throws Exception {
-		final String id = UUID.randomUUID().toString().replace("-", "");
-		String uri = "/resource/"+id+"/action?";
-		final Random random = new Random();
-		final Map<String, String> expected = new HashMap<String, String>();
-		final int count = 17;
-		for (int i = 0; i < count; i++) {
-			final String key = String.valueOf(random.nextDouble());
-			final String value = String.valueOf(random.nextDouble());
-			expected.put(key, value);
-			uri += key + "=" + value;
-			if (i != count-1) uri += "&";
-		}
-		
-		final Map<String, String> arguments = URIParser.instance.parseURIArguments(uri);
-		final Iterable<String> keys = arguments.keySet();
-		for (final String key : keys) {
-			final String value = arguments.get(key);
-			final String expectedvalue = expected.get(key);
-			assertEquals(expectedvalue, value);
-		}
-	}
-	
 	public void testURIFull() {
 		final String id = UUID.randomUUID().toString().replace("-", "");
 		final String uri = "/resource/"+id+"/action";
