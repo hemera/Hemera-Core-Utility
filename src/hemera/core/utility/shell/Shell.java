b10c6a5e3a0639638b539178453f8434660a0e68
Yi Wang <neakor@gmail.com>
2013-07-31 23:06:00 -0400

    Updated shell to use safer command array option.

--- src/hemera/core/utility/shell/Shell.java
+++ src/hemera/core/utility/shell/Shell.java
@@ -10,42 +10,51 @@ import java.io.IOException;
  * shell command.
  *
  * @author Yi Wang (Neakor)
- * @version 1.0.0
+ * @version 1.0.6
  */
 public enum Shell {
 	/**
 	 * The singleton instance.
 	 */
 	instance;
-
+	
 	/**
-	 * Execute the given command.
-	 * @param command The <code>String</code> command.
+	 * Execute the given command array.
+	 * @param command The <code>String</code> array of
+	 * command.
 	 * @return The execution <code>ShellResult</code>.
 	 * @throws IOException If an I/O error occurred.
 	 * @throws InterruptedException If waiting for the
 	 * execution to complete was interrupted.
 	 */
-	public ShellResult execute(final String command) throws IOException, InterruptedException {
-		final Process process = Runtime.getRuntime().exec(command);
+	public ShellResult execute(final String[] command, final boolean asRoot) throws IOException, InterruptedException {
+		Process process = null;
+		if (asRoot) {
+			final String[] array = new String[command.length+1];
+			array[0] = "sudo";
+			System.arraycopy(command, 0, array, 1, command.length);
+			process = Runtime.getRuntime().exec(array);
+		} else {
+			process = Runtime.getRuntime().exec(command);
+		}
+		return this.readResult(process);
+	}
+	
+	/**
+	 * Read the shell result from given process.
+	 * @param process The <code>Process</code> to read.
+	 * @return The <code>ShellResult</code> instance.
+	 * @throws IOException If an I/O error occurred.
+	 * @throws InterruptedException If waiting for the
+	 * execution to complete was interrupted.
+	 */
+	private ShellResult readResult(final Process process) throws IOException, InterruptedException {
 		final String output = FileUtils.instance.readAsString(process.getInputStream());
 		final String error = FileUtils.instance.readAsString(process.getErrorStream());
 		final StringBuilder builder = new StringBuilder();
 		builder.append(output).append("\n").append(error);
 		final int code = process.waitFor();
 		return new ShellResult(code, builder.toString());
-	}
-	
-	/**
-	 * Execute the given command with root privilege.
-	 * @param command The <code>String</code> command.
-	 * @return The execution <code>ShellResult</code>.
-	 * @throws IOException If an I/O error occurred.
-	 * @throws InterruptedException If waiting for the
-	 * execution to complete was interrupted.
-	 */
-	public ShellResult executeAsRoot(final String command) throws IOException, InterruptedException {
-		return this.execute("sudo " + command);
 	}
 	
 	/**
@@ -57,7 +66,7 @@ public enum Shell {
 	 * is interrupted.
 	 */
 	public void makeExecutable(final String target) throws IOException, InterruptedException {
-		final ShellResult result = this.execute("chmod +x " + target);
+		final ShellResult result = this.execute(new String[] {"chmod", "+x", target}, true);
 		if (result.code != 0) throw new IOException("Making hemera script executable failed.\n" + result.output);
 	}
 }
